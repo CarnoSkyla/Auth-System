d328728cbfc6c0cacdf70fba2e541ea019f8d6b8
Sibusiso Labi <49154656+CarnoSkyla@users.noreply.github.com>
2022-10-14 15:46:33 +0200

    express setup

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const router = require('./routes/auth');
 
 const app = express();
+
+const authRoutes = router;
 
 app.get('/', (req, res) => {
     res.send('Welcome to the Auth System')
 })
 
+app.use('/api/users', authRoutes)
+
 app.listen(3000, () => {
     console.log('Server is running');
-})
+}) 
